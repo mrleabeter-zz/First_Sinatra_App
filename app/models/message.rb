8772d91b5541c9339bc79e3e3a86ec5ae15b794e
Christopher Leabeter <christopherleabeter@gmail.com>
2015-09-14 19:39:46 -0400

    Completed Enhance Task 2 Bonus

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
 
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :author, presence: true
   validates :content, presence: true
-
+  validates :url, format: { with: /\Ahttp:\/{2}w{3}/, message: "must begin with 'http://www'"}
 
 end
