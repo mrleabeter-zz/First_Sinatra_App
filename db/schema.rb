a8e6cf652d75d56c229ff6f46e876a81c06192f1
Christopher Leabeter <christopherleabeter@gmail.com>
2015-09-14 17:58:09 -0400

    Enhancement 1 and 2 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914170145) do
+ActiveRecord::Schema.define(version: 20150914175200) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "title"
     t.string "author"
     t.string "content"
+    t.text   "url"
   end
 
 end
